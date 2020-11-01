35cc93821b47fc512b4fbcd07ed53dedd261e79d
Yankang <yankang.zhu@icloud.com>
2020-11-01 20:30:04 +0000

    add comments

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
+/*
+    main.cpp
+    1. Deomstrate how to place, modify, confirm trade though member functions of Portfolio class.
+*/
+
 #include <iostream>
 #include <vector>
 #include "Message.h"
 #include "Portfolio.h"
 using namespace std;
-enum MESSAGE_TYPE
-{
-    ORDER = 1,
-    DELETE,
-    MODIFY,
-    TRADE,
-};
+
 enum STOCKS
 {
     A,
@@ -19,23 +18,24 @@ enum STOCKS
 int main()
 {
     /* Test serialize and deserialise */
-    // Header h1(132, 172, 258427), h2;
-    // // messageType=1, listingID, orderId, orderQuantity, orderPrice, side
-    // NewOrder no1(12, 1, 20, 1500, 'b'), no2;
-    // uint8_t buffer[16 + 35] = {0};
-    // h1.serialize(buffer);
-    // no1.serialize(buffer);
-
-    // cout << h1;
-    // cout << no1;
-    // cout << endl;
-    // cout << "main: size " << sizeof(buffer);
-    // h2.deserialize(buffer);
-    // no2.deserialize(buffer);
-    // cout << h2;
-    // cout << no2;
+    cout << "----Testing Serialisation----\n";
+    Header h1(132, 172, 258427), h2;
+    NewOrder no1(12, 1, 20, 1500, 'b'), no2; // messageType=1, listingID, orderId, orderQuantity, orderPrice, side
+    uint8_t buffer[16 + 35] = {0};
+    h1.serialize(buffer);
+    no1.serialize(buffer);
+    cout << "Before serialisation----\n";
+    cout << h1;
+    cout << no1;
+    cout << "After serialisation----\n";
+    h2.deserialize(buffer);
+    no2.deserialize(buffer);
+    cout << h2;
+    cout << no2;
+    cout << endl;
 
     /* Test place_order */
+    cout << "----Placing Orders----\n";
     uint8_t order_id = 0;
     Portfolio p(20, 15);
     vector<NewOrder> orders;
@@ -48,23 +48,24 @@ int main()
     // Place more
     cout << p.place_order(NewOrder(A, order_id++, 10, 1500, 's'));
     cout << p.place_order(NewOrder(A, order_id++, 10, 1500, 'b'));
+    cout << endl;
     // Modify Order
-    cout << endl
+    cout << "----Modifying Order (id 1)----\n"
          << "before modification" << endl
-         << p.orders[1];
-    cout << endl
-         << "modify to 6\n";
+         << p.orders[1]
+         << endl
+         << "modify quantity to 6\n";
     p.modify_order(ModifyOrderQuantity(1, 6));
     cout << p.orders[1] << endl;
 
     cout << endl
-         << "modify to 13\n";
+         << "modify quantity to 13 (Rejected)\n"; // Rejectd, too high.
     p.modify_order(ModifyOrderQuantity(1, 13));
     cout << p.orders[1] << endl;
 
     // Confirm Trade
-    cout << "confirm trade" << endl;
-    p.confirm_trade(Trade(A, 1, 3, 1500));
-    cout << p.place_order(NewOrder(A, order_id++, 3, 1500, 'b')); //Unsuccessful.
+    cout << "----confirm trade (sell)----\n";
+    p.confirm_trade(Trade(A, 2, 3, 1500));
+    cout << p.place_order(NewOrder(A, order_id++, 3, 1500, 's')); //Rejected.
     return 0;
 }
