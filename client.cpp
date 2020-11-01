f4a288c8d3fa3f803f1b8e8a7b11fd84c806474a
Yankang <yankang.zhu@icloud.com>
2020-11-01 18:56:58 +0000

    client send threshold

--- client.cpp
+++ client.cpp
@@ -8,19 +8,52 @@
 #include <netdb.h>
 #include "Message.h"
 
+enum STOCKS
+{
+    FLOW,
+    OTHER,
+};
+
 void error(const char *msg)
 {
     perror(msg);
     exit(0);
 }
 
+void send_buffer(uint8_t *buffer, int sockfd)
+{
+    // Send buffer
+    int n = write(sockfd, buffer, 64);
+    if (n < 0)
+        error("ERROR writing to socket");
+}
+
+void recive_buffer(uint8_t *buffer, int sockfd)
+{
+    // Recive response.
+    int n = read(sockfd, buffer, 255);
+    if (n < 0)
+        error("ERROR reading from socket");
+}
+
+void process(int sockfd)
+{
+    int order_id = 0;
+    int sequence_num = 0;
+    uint8_t buffer[64];
+    Header hearder1(AF_INET, 35, sequence_num++); //NewOrder is size 35;
+    NewOrder order1(FLOW, order_id++, 15, 1500, 's');
+    hearder1.serialize(buffer);
+    order1.serialize(buffer);
+    send_buffer(buffer, sockfd);
+}
+
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
+    /* Establish TCP connection. */
+    int sockfd, portno;
     struct sockaddr_in serv_addr;
     struct hostent *server;
-
-    // char buffer[256];
     if (argc < 3)
     {
         fprintf(stderr, "usage %s hostname port\n", argv[0]);
@@ -43,27 +76,16 @@ int main(int argc, char *argv[])
     if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
         error("ERROR connecting");
 
-    unsigned char buffer[256] = {0};
-    char temp[256] = {0};
-    Header h(42, 422, 220284);
+    /* Sending buy and sell thresholds. */
+    uint8_t buffer[16];
+    Threshold threshold;
+    threshold.get_threshold();
+    threshold.serialize(buffer);
+    send_buffer(buffer, sockfd);
+    // process(sockfd);
+    // while (1)
+    // {
 
-    while (1)
-    {
-
-        printf("Please enter the message: ");
-        bzero(buffer, 256);
-        fgets(temp, 255, stdin);
-        h.serialize(buffer);
-        // memcpy(buffer, buffer, 16);
-        n = write(sockfd, buffer, 16);
-        if (n < 0)
-            error("ERROR writing to socket");
-        bzero(buffer, 256);
-        n = read(sockfd, buffer, 255);
-        if (n < 0)
-            error("ERROR reading from socket");
-        printf("%s\n", buffer);
-    }
     close(sockfd);
     return 0;
 }
